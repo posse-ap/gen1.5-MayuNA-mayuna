4c1c5263228a4cfb6dc0bd8bfe5219943573203b
MayuNA-mayuna <74807676+MayuNA-mayuna@users.noreply.github.com>
2021-08-22 18:41:04 +0900

    phpadminまで

--- Dockerpractice2/src/index.php
+++ Dockerpractice2/src/index.php
@@ -1,21 +1,5 @@
-
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>kuizy</title>
-     <link rel ="stylesheet" href="./index.css">
-  </head>
-  
-  <body>
-        <div class="loop1" id="loop1">
-            <script src="./index.js"></script>
-        </div>
-  </body>
-
-  
-</html> 
-    
-    
-
+<meta charset="UTF-8">
+<title>チャイ</title>
+<?php
+phpinfo();
+?>
