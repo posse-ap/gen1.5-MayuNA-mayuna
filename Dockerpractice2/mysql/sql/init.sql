77122750a104d77846d5d49e8db5bb89e974fda7
MayuNA-mayuna <74807676+MayuNA-mayuna@users.noreply.github.com>
2021-09-19 14:13:59 +0900

    quizy完成

--- Dockerpractice2/mysql/sql/init.sql
+++ Dockerpractice2/mysql/sql/init.sql
@@ -10,4 +10,13 @@ CREATE TABLE questions (
 
 INSERT INTO questions (id,name) VALUES ('1', '東京'), ('2', '広島県');
 
+DROP TABLE IF EXISTS choices;
+CREATE TABLE choices (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  question_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(225) NOT NULL,
+  valid INT AUTO_INCREMENT NOT NULL PRIMARY KEY
+);
 
+
+INSERT INTO choices (id, question_id, name, valid) VALUES (1, 1, 'たかなわ', 1), (1, 2, 'たかわ', 0);
--- Dockerpractice2/src/index.php
+++ Dockerpractice2/src/index.php
@@ -17,6 +17,23 @@ try {
   
   $questions = $stmt->fetch();
 
+  // print_r($questions);
+
+  
+
+  $stmt = $pdo->prepare('SELECT * FROM choices WHERE  id = :id');
+  $stmt->bindParam(':id', $_GET['id']);
+  $stmt->execute();
+
+  $choices = $stmt->fetchAll();
+
+
+
+  // print_r($choices[0]['valid']);
+
+
+
+  // print_r($choices);
 } catch (PDOException $e) {
   echo $e->getMessage() . PHP_EOL;
   exit;
@@ -33,9 +50,44 @@ try {
   
   <body>
   <h1><?php echo $questions['name']?>の難読地名クイズ</h1>
-        <div class="loop1" id="loop1">
-            <script src="./index.js"></script>
+        <!-- <div class="loop1" id="loop1"> -->
+        <div class="all">
+          <img src="<?php echo $questions['name']?>.png" class="image">
+          <span class="under_line">1.この地名はなんて読む？</span><div class="green"></div>
+
+          <?php 
+
+          $number = 0;
+          //TODO　　最後にこれ($number)をarraynumberにする
+          // 東京の選択肢０、１、２
+          // 広島の選択肢０、１、２
+
+          $question_id1 = $choices[$number]['question_id'];
+          $question_id2 = $choices[$number + 1]['question_id'];
+          $question_id3 = $choices[$number + 2]['question_id'];
+
+          // $valid_id1 = $choices[$number]['valid'];
+          // $valid_id2 = $choices[$number + 1]['valid'];
+          // $valid_id3 = $choices[$number + 2]['valid'];
+
+          $question_number = $choices[$number]['question_number'];
+          $valid_num = 1;
+
+
+          ?>
+
+          <li class="button" id ="choice_<?php echo $question_id1?>_<?php echo $question_number?>" onclick=check(<?php echo $question_id1?>,<?php echo $valid_num?>,<?php echo $question_number?>) name="choice_<?php echo $question_number?>" ><?php echo $choices[0]['name'] ?></li>
+          <li class="button" id ="choice_<?php echo $question_id2?>_<?php echo $question_number?>" onclick=check(<?php echo $question_id2?>,<?php echo $valid_num?>,<?php echo $question_number?>) name="choice_<?php echo $question_number?>" ><?php echo $choices[$number + 1]['name'] ?></li>
+          <li class="button" id ="choice_<?php echo $question_id3?>_<?php echo $question_number?>" onclick=check(<?php echo $question_id3?>,<?php echo $valid_num?>,<?php echo $question_number?>) name="choice_<?php echo $question_number?>" ><?php echo $choices[$number + 2]['name'] ?></li>
+          <!-- <div id="answerbox_' + question_id + '" class="box"> -->
+            <div class="box" id = "answer_box_<?php echo $question_number?>">
+              <p id = "correct_result_<?php echo $question_number?>"></p><br><p>正解は<?php echo $choices[$number]['name']?>です</p> 
+            </div>
+          <!-- </div> -->
         </div>
+
+        <script src="contents.js"></script>
+        <!-- </div> -->
   </body>
 
   
