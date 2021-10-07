bdc61b8a6c66421875bb50247f46597dd90e5309
MayuNA-mayuna <74807676+MayuNA-mayuna@users.noreply.github.com>
2021-10-07 13:42:54 +0900

    shuffle追加、css整え

--- Dockerpractice2/src/index.php
+++ Dockerpractice2/src/index.php
@@ -43,34 +43,30 @@ try {
   <h1><?php echo $questions[0]['name'] ?>の難読地名クイズ</h1>
         <!-- <div class="loop1" id="loop1"> -->
         <?php for($i=0;$i<count($choices)/3;$i++){?>
+
+        <?php $stmt = $pdo->prepare('SELECT * FROM choices WHERE question_number = :question_number AND id = :id');
+        $j = $i + 1;
+        $stmt->bindParam(':question_number',$j);
+        $stmt->bindParam(':id', $_GET['id']);
+        $stmt->execute();
+        
+        $each_question = $stmt->fetchAll();
+        
+        shuffle($each_question);
+
+        $valid_num = 1;
+        ?>
+
         <div class="all">
-          <img src="<?php echo $_GET['id']?>_<?php echo $i+1?>.png" class="image">
+          <img src="img/<?php echo $_GET['id']?>_<?php echo $i+1?>.png" class="image">
           <span class="under_line"><?php echo $i+1?>.この地名はなんて読む？</span><div class="green"></div>
 
-          <?php 
+          <?php foreach($each_question as $each_choice){?>
+            <li class="button" id ="choice_<?php echo $each_choice['question_id']?>_<?php echo $each_choice['question_number']?>" onclick=check(<?php echo $each_choice['question_id']?>,<?php echo $valid_num?>,<?php echo $each_choice['question_number']?>) name="choice_<?php echo $each_choice['question_number']?>" ><?php echo $each_choice['name'] ?></li>
+          <?php } ?>
 
-          $number = $i*3;
-          //TODO　　最後にこれ($number)をarraynumberにする
-          // 東京の選択肢０、１、２
-          // 広島の選択肢０、１、２
-
-          $question_id1 = $choices[$number]['question_id'];
-          $question_id2 = $choices[$number + 1]['question_id'];
-          $question_id3 = $choices[$number + 2]['question_id'];
-
-
-          $question_number = $choices[$number]['question_number'];
-          
-          $valid_num = 1;
-
-          ?>
-
-          <li class="button" id ="choice_<?php echo $question_id1?>_<?php echo $question_number?>" onclick=check(<?php echo $question_id1?>,<?php echo $valid_num?>,<?php echo $question_number?>) name="choice_<?php echo $question_number?>" ><?php echo $choices[$number]['name'] ?></li>
-          <li class="button" id ="choice_<?php echo $question_id2?>_<?php echo $question_number?>" onclick=check(<?php echo $question_id2?>,<?php echo $valid_num?>,<?php echo $question_number?>) name="choice_<?php echo $question_number?>" ><?php echo $choices[$number + 1]['name'] ?></li>
-          <li class="button" id ="choice_<?php echo $question_id3?>_<?php echo $question_number?>" onclick=check(<?php echo $question_id3?>,<?php echo $valid_num?>,<?php echo $question_number?>) name="choice_<?php echo $question_number?>" ><?php echo $choices[$number + 2]['name'] ?></li>
-          <!-- <div id="answerbox_' + question_id + '" class="box"> -->
-            <div class="box" id = "answer_box_<?php echo $question_number?>">
-              <p id = "correct_result_<?php echo $question_number?>"></p><br><p>正解は<?php echo $choices[$number]['name']?>です</p> 
+            <div class="box" id = "answer_box_<?php echo $each_choice['question_number']?>">
+              <p id = "correct_result_<?php echo $each_choice['question_number']?>"></p><br><p>正解は<?php echo $choices[$i*3]['name']?>です</p> 
             </div>
           <!-- </div> -->
         </div>
