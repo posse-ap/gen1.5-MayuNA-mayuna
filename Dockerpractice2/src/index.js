34397e659de53ab8435d7ed85e5af7c1cbf34463
MayuNA-mayuna <74807676+MayuNA-mayuna@users.noreply.github.com>
2021-10-07 21:49:22 +0900

    一通り完成です

--- Dockerpractice2/src/index.js
+++ Dockerpractice2/src/index.js
@@ -1,16 +1,11 @@
 'use strict' ;
 
 
-var success1 = document.createTextNode('正解!');
-var false1 = document.createTextNode('不正解!');
 
-
-//question_id ：１問題の中のそれぞれのid
-//valid_id:あっているものが１間違いが０
-//id 問題ごとid
 function check(question_id,valid_num,question_number){
 
   var correct_result = document.getElementById('correct_result_' + question_number );
+  console.log(correct_result);
   //クリック無効化
     var answerlists = document.getElementsByName('choice_' + question_number);
     answerlists.forEach(answerlist => {
@@ -26,12 +21,12 @@ function check(question_id,valid_num,question_number){
 
 
       if(question_id == valid_num){
-        correct_result.appendChild(success1);
+        correct_result.innerText = '正解！';
         correct_result.classList.add('ansT');
         // box.classList.add('click_box');
       }else{
         button_red.classList.add('red'); 
-        correct_result.appendChild(false1);
+        correct_result.innerText = '不正解！';
         correct_result.classList.add('ansF');
       }
 answer_box.style.display = 'block';
