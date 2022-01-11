f8b8bcd874210ac4dbbc2b4ff09abd05244453cf
MayuNA-mayuna <74807676+MayuNA-mayuna@users.noreply.github.com>
2022-01-11 13:23:12 +0900

    ログイン機能完成bootstrapが効かない

--- posse-ph3-webapp/routes/web.php
+++ posse-ph3-webapp/routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
